46802945d8b3fffc2994e9d74523528dc0332a4b
Korawit Rupanya <32285708+KorawitRupanya@users.noreply.github.com>
2023-11-10 20:37:00 +0100

    Fix monitor

--- UPISAS/tests/your_exemplar/test_your_exemplar_interface.py
+++ UPISAS/tests/your_exemplar/test_your_exemplar_interface.py
@@ -60,7 +60,7 @@ class TestYourExemplarInterface(unittest.TestCase):
     def test_schema_of_execute(self):
         self.strategy.get_execute_schema()
         with self.assertLogs() as cm:
-            successful = self.strategy.execute({"server_number": 2, "dimmer_factor": 0.5})
+            successful = self.strategy.execute({"config": "|../repository/TCPNetwork.o,/home/roberto/dana//components/net/TCP.o,../repository/request/RequestHandlerPT.o,../repository/app_protocols/HTTPProtocol.o,../repository/http/HTTPHeader1_0.o,../repository/http/handler/GET/HTTPGETCMP.o,/home/roberto/dana//components/io/File.o,../repository/compression/ZLIB.o,/home/roberto/dana//components/os/Run.o,/home/roberto/dana//components/time/DateUtil.o,../repository/http/util/HTTPUtil.o,/home/roberto/dana//components/data/StringUtil.o,/home/roberto/dana//components/data/adt/List.o|0:net.TCPSocket:1,0:net.TCPServerSocket:1,0:request.RequestHandler:2,2:app_protocols.AppProtocol:3,3:http.HTTPHeader:4,4:http.handler.GET.HTTPGET:5,5:io.File:6,5:compression.Compression:7,7:os.Run:8,7:time.DateUtil:9,7:io.FileSystem:6,4:http.util.HTTPUtil:10,10:io.FileSystem:6,10:data.StringUtil:11,11:data.adt.List:12|"})
             self.assertTrue("JSON object validated by JSON Schema" in ", ".join(cm.output))
         self.assertTrue(successful)
 
