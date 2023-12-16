7dc5934cee9777fe91abb7eb8c547f1d451518d8
nashid <nashid4722@gmail.com>
2023-12-16 17:37:16 +0100

    add the compositon architecture

--- UPISAS/strategies/empty_strategy.py
+++ UPISAS/strategies/empty_strategy.py
@@ -8,3 +8,47 @@ class EmptyStrategy(Strategy):
 
     def plan(self):
         return True
+
+    def getInfoArchitecture(arch):
+        request = getRequest(arch)
+        defGet = getGET(arch)
+        cmp = getCMP(arch)
+        ch = getCH(arch)
+        result = []
+        if request is not None:
+            result.append(request)
+        if defGet is not None:
+            result.append(defGet)
+        if cmp is not None:
+            result.append(cmp)
+        if ch is not None:
+            result.append(ch)
+        return result
+
+    def getRequest(arch):
+        compsList = arch.split(',')
+        for comp in compsList:
+            if comp.startswith('..repository/request/'):
+                return comp.split('/')[-1]
+        return None
+
+    def getGET(arch):
+        compsList = arch.split(',')
+        for comp in compsList:
+            if comp.startswith('..repository/http/handler/GET/'):
+                return comp.split('/')[-1]
+        return None
+
+    def getCMP(arch):
+        compsList = arch.split(',')
+        for comp in compsList:
+            if comp.startswith('..repository/compression/'):
+                return comp.split('/')[-1]
+        return None
+
+    def getCH(arch):
+        compsList = arch.split(',')
+        for comp in compsList:
+            if comp.startswith('..repository/cache/'):
+                return comp.split('/')[-1]
+        return None
